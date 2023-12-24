bbaf18b950ca8fa3304ada74af7eb44e9594b8a5
Kassen Qian <kassen.qian@datadoghq.com>
2023-12-24 11:24:39 -0700

    first chapter page setup

---
+++ book/1-begin/pages/_app.jsx
@@ -0,0 +1,22 @@
+import App from 'next/app';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    // console.log(pageProps);
+
+    return <Component {...pageProps} />;
+  }
+}
+
+MyApp.propTypes = propTypes;
+
+export default MyApp;
