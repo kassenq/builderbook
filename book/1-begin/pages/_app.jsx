d45f1c10990f68668b5fedc39110976e2e1fdc86
Kassen Qian <kassen.qian@datadoghq.com>
2023-12-24 11:53:38 -0700

    end chapter 1

--- book/1-begin/pages/_app.jsx
+++ book/1-begin/pages/_app.jsx
@@ -1,6 +1,15 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import App from 'next/app';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Head from 'next/head';
+
+import { theme } from '../lib/theme';
+
+import Header from '../components/Header';
 
 const propTypes = {
   Component: PropTypes.elementType.isRequired,
@@ -13,7 +22,18 @@ class MyApp extends App {
 
     // console.log(pageProps);
 
-    return <Component {...pageProps} />;
+    return (
+      <CacheProvider value={createCache({ key: 'css' })}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <CssBaseline />
+          <Header {...pageProps} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    );
   }
 }
 
--- book/1-begin/pages/index.jsx
+++ book/1-begin/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/Header';
+import Button from '@mui/material/Button';
 
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
@@ -7,8 +7,8 @@ const Index = () => (
       <title>Index page</title>
       <meta name="description" content="This is the description of the Index page" />
     </Head>
-    <Header />
     <p>Content on Index page</p>
+    <Button variant="contained">MUI button</Button>
   </div>
 );
 
